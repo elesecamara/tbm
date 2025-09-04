78035d4fd3745dd34596b3551c85c2ae525b8a4a
Sergio Camara <oigresaramac@gmail.com>
2025-09-03 20:34:48 -0600

    ajustes responsivos y loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoaderComponent } from './components/loader/loader.component';
 import { HeaderComponent } from "./components/header/header.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoaderComponent, HeaderComponent],
+  imports: [RouterOutlet, LoaderComponent, HeaderComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
